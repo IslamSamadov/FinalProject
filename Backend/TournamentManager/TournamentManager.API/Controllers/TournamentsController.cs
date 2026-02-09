1642e40e206312491ca024c8f235a82b1fedf382
IslamSamadov <islamsamedov52@gmail.com>
2026-02-09 23:15:27 +0400

    Folder Structure Done

---
+++ Backend/TournamentManager/TournamentManager.API/Controllers/TournamentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TournamentManager.API.Data;
+
+namespace TournamentManager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TournamentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TournamentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
--- Backend/TournamentManager/TournamentManager.API/Program.cs
+++ Backend/TournamentManager/TournamentManager.API/Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using TournamentManager.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
@@ -17,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
