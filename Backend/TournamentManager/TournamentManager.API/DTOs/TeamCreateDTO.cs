679dffa92ef37a24ef5316805311df142675a5ae
IslamSamadov <islamsamedov52@gmail.com>
2026-02-19 23:36:44 +0400

    Added TeamCreateDto.

---
+++ Backend/TournamentManager/TournamentManager.API/DTOs/TeamCreateDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TournamentManager.API.DTOs
+{
+    public class TeamCreateDTO
+    {
+        [Required(ErrorMessage = "Team name is required.")]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int TournamentId { get; set; }
+    }
+}
